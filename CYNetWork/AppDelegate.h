de5078c7d7d6ed76c3d6ca4ace78065daac682fe
chengyi.gao <chengyi.gao@weimob.com>
2018-07-05 10:34:58 +0800

    首次创建

---
+++ CYNetWork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CYNetWork
+//
+//  Created by 高程宜 on 2018/7/5.
+//  Copyright © 2018年 高程宜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CYNetWork/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CYNetWork
+//
+//  Created by 高程宜 on 2018/7/5.
+//  Copyright © 2018年 高程宜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
